5f7c641570f3e39fdfa816f1fc66c8b5e13b4b6b
ckordes <ckordes@gmail.com>
2019-05-12 15:22:46 +0200

    zadanie5

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,12 +1,20 @@
 package pl.coderslab.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.model.Book;
+import pl.coderslab.model.MemoryBookService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    @Autowired
+    MemoryBookService memoryBookService;
+
     @RequestMapping("/hello")
     public String hello() {
         return "{hello: World}";
